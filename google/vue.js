305371ccaeb91d785f6019c5fd2eada1abd0035e
apietryka886 <apietryka886gmail.com>
2021-04-26 20:28:15 +0200

    zadanie 7

--- google/vue.js
+++ google/vue.js
@@ -2,32 +2,55 @@ var app = new Vue({
     el: "#app",
     data: {
         googleSearch: "",
-        cities: window.cities
+        cities: window.cities,
+        isActive: 0,
+        control: 0
     },
     updated() {
-
         this.$nextTick(() => {
-        
-        if (this.googleSearch.length > 0) {
-        
+        if (this.googleSearch.length > 0) {        
         this.$refs.second.focus();
-        
         } else {
-        
         this.$refs.first.focus();
-        
         }
-        
         });
-        
     },
     computed: {
-        filtredCities: function(){
-            let result = this.cities.filter(city => city.name.includes(this.googleSearch));
-            if(result.length > 10){
-                return result.slice(0, 10);
+        filteredCities: function(){
+            if(this.googleSearch.length > 0 ){
+                let result = this.cities.filter(city => city.name.includes(this.googleSearch));
+                if(result.length > 10){
+                    return result.slice(0, 10);
+                }
+                return result;
             }
-            return result;
+        }
+    },
+    methods:{
+        zmiana: function(a)
+        {
+            if(this.isActive == 0)
+            {
+                this.isActive = 1;
+                this.googleSearch = a;
+                el2 = document.getElementById("autocom");
+                el2.blur();
+                this.control = 0;
+            }
+        },
+        pogrubienie: function(a)
+        {
+            wyszukaj = this.googleSearch;
+            var pom = a.split(wyszukaj);
+            for(i = 0; i < pom.length; i++)
+            {
+                a = a.replace(pom[i], pom[i].bold());
+            }
+            return a;
+        },
+        ustaw: function()
+        {
+            this.control = 1;
         }
     }
 });
